ce72ea64a3c8e9b4004c71a3019c8f9b7446454c
Spyros Barbakos <spirosbarbakos7@gmail.com>
2024-10-09 10:37:10 +0300

    inferenced models on the second part of the dataset

---
+++ insomnia_crawler/run_allgreek2me.py
@@ -0,0 +1,38 @@
+import allgreek2me_master.greeklish as greeklish
+import os 
+import json
+
+
+forums = os.listdir("forums_info/forums_sampled2")
+g2g = greeklish.GreeklishConverter()
+
+for forum in forums:
+    with open(f"forums_info/forums_sampled2/{forum}") as f:
+        forum_data = json.load(f)
+
+    transliterated_data = []
+
+    # Iterate over the posts
+    for post in forum_data:
+
+        # Convert the text
+        predicted_text = g2g.convert(post['text'])
+        
+
+        # Add the transliterated data to the list
+        transliterated_data.append({
+            "greeklish": post["text"],
+            "greek": predicted_text,
+            "gt_indices": post["gt_indices"]
+        })
+        print(predicted_text)
+
+    folder_path = "LLMs/LLM_data/allgreek2me_data2/"
+    # Make the dir {model}_data if it doesn't exist
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    print(f"{folder_path}{forum}")
+    # Save the transliterated data
+    with open(f"{folder_path}/{forum}", "w") as f:
+        json.dump(transliterated_data, f, ensure_ascii=False, indent=4)
